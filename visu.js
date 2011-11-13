c9933b51cadc0df95284463aff1744f2ab22e83a
Georges Gouriten <georges.gouriten@gmail.com>
2011-11-14 00:42:08 +0100

    Saves an array every minute with twitts and rates

--- visu.js
+++ visu.js
@@ -35,63 +35,75 @@ var https = require('https');
 var pass = process.env.TWITTER_PASS || 'username:password';
 var auth = 'Basic ' + new Buffer(pass).toString('base64');
 
-var databaseURL = process.env.MONGOHQ_URL || 'dataviz';
-var db = require('mongojs').connect(databaseURL, ['rates']);
+var politiciansMetabaseURL = process.env.MONGOHQ_URL || 'politiciansMetaviz';
+var db = require('mongojs').connect(politiciansMetabaseURL, ['twitts']);
 
-var data = {
+var politiciansMeta = {
   "nodes": [
-    { "name":"Nicolas Sarkozy", "keywords": ["sarkozy", "nsarkozy", "nicolassarkozy"], "value":0 },
-    { "name":"Eva Joly", "keywords": ["joly", "ejoly", "evajoly"], "value":0 },
-    { "name":"François Hollande", "keywords": ["hollande", "fhollande", "francoishollande"], "value":0 }
-/*
-    { "name":"Merkel", "value":0 },
-    { "name":"Obama", "value":0 },
-    { "name":"Mélenchon", "value":0 },
-    { "name":"Le Pen", "value":0 },
-    { "name":"Dupont Aignant", "value":0 },
-    { "name":"Natalie Arthau", "value":0 },
-    { "name":"Chevenement", "value":0 },
-    { "name":"Boutin", "value":0 },
-    { "name":"Nétanyahou", "value":0 },
-    { "name":"Papandreou", "value":0 },
-    { "name":"Berlusconi", "value":0 },
-    //{ "name":"Bieber", "value":0 },
-    //{ "name":"Steve Jobs", "value":0 },
-*/
+    { "name":"Nicolas Sarkozy", "keywords": ["sarkozy", "nsarkozy", "nicolassarkozy"] },
+    { "name":"Eva Joly", "keywords": ["joly", "ejoly", "evajoly"] },
+    { "name":"François Hollande", "keywords": ["hollande", "fhollande", "francoishollande"] }
+		/*{ "name":"Jean-Luc Mélenchon", "keywords": ["melenchon", "jlmelenchon"] },
+		{ "name":"François Bayrou", "keywords": ["bayrou", "fbayrou", "francoisbayrou"] },
+		{ "name":"Nicolas Dupont-Aignan", "keywords": ["dupont-aignan"] },
+		{ "name":"Philippe Poutou", "keywords": ["poutou"] },
+		{ "name":"Corine Lepage", "keywords": ["corine+lepage"] },
+		{ "name":"Christine Boutin", "keywords": ["christine+boutin"] },
+		{ "name":"Frédéric Nihous", "keywords": ["frederic+nihous"] }*/
   ]
 }
 
-setInterval(function(){
-  var to_save = new Array();
-  for(var i in data.nodes) {
-    if(data.nodes[i].value > 0) {
-      to_save.push({'name':data.nodes[i].name, 'value':data.nodes[i].value, 'date': new Date()});
-      data.nodes[i].value = 0;
-    } 
-  }
-  for(var i in to_save){
-    db.rates.save(to_save[i]);
-  }
-}, 60*1000)
+//Array containing the twitter activity per minute
+var perMinuteActivity = {"nodes": []};
 
+for(var i in politiciansMeta.nodes){
+	var politicianName = politiciansMeta.nodes[i].name;
+	perMinuteActivity.nodes.push({"name": politicianName, "count": 0, "twitts": [""]});
+}
+
+var perHourActivity = [];
+
+//Saving perMinutArray in the mongodb
+setInterval(function(){
+  db.twitts.save(perMinuteActivity);
+	console.log("------------Pushed---------------")
+	console.log(perMinuteActivity.nodes[0])
+	console.log(perMinuteActivity.nodes[1])
+	console.log(perMinuteActivity.nodes[2])
+
+	for(var i in perMinuteActivity.nodes){
+		perMinuteActivity.nodes[i].count = 0;
+		try {
+			perMinuteActivity.nodes[i].twitts = [""];
+		} catch(err) { console.log(err);}
+	}
+}, 10*1000)
+
+//building the api streaming query
 var query = "";
-for(var i in data.nodes){
-	for(var j in data.nodes[i].keywords)
-  query+=encodeURIComponent(data.nodes[i].keywords[j])+",";
+for(var i in politiciansMeta.nodes){
+	for(var j in politiciansMeta.nodes[i].keywords){
+  	query+=encodeURIComponent(politiciansMeta.nodes[i].keywords[j])+",";
+	}
 }
 query=query.slice(0, query.length-1);
 console.log(query);
+
+//api streaming
 https.get({ host: 'stream.twitter.com', path: '/1/statuses/filter.json?track='+query, headers:{'Authorization': auth} }, function(res) {
 
   res.on('data', function(d) {
     try{
       var o = JSON.parse(d);
       var detected = false;
-      for(var i in data.nodes) {
-				for(var j in data.nodes[i].keywords)
-        	if(o.text.toLowerCase().indexOf(data.nodes[i].keywords[j].toLowerCase())!=-1){
-          	console.log(data.nodes[i].keywords[j] + " détecté dans: "+o.text);
-          	data.nodes[i].value += 1;
+      for(var i in politiciansMeta.nodes) {
+				for(var j in politiciansMeta.nodes[i].keywords)
+        	if(o.text.toLowerCase().indexOf(politiciansMeta.nodes[i].keywords[j].toLowerCase())!=-1){
+						var politicianName = politiciansMeta.nodes[i].name
+						perMinuteActivity.nodes[i].count++;
+          	perMinuteActivity.nodes[i].twitts.push({"text": o.text, "user": o.user.screen_name});
+						console.log("------------New detection: "+ politiciansMeta.nodes[i].keywords[j] + "---------------")		
+						console.log(perMinuteActivity);
           	detected = true;
 						break;
         	}
@@ -100,7 +112,7 @@ https.get({ host: 'stream.twitter.com', path: '/1/statuses/filter.json?track='+q
         console.error('n\'a rie détecté dans '+o.text);
       }
       else{
-        //io.sockets.emit('refresh data', data);
+        //io.sockets.emit('refresh politiciansMeta', politiciansMeta);
         //io.sockets.emit('last tweet', o.text);
       }
     }
